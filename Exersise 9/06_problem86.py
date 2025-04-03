7b9a04eeb2855eb6899012ea4f5e534a81421e54
Abhi-sarkar-tech <abhiisarkar6038@gmail.com>
2025-04-03 19:12:46 +0530

    add new problems

---
+++ Exersise 9/06_problem86.py
@@ -0,0 +1,17 @@
+## Hello Hill Pattern
+
+n = 9
+for i in range(n):
+    for j in range(i, n):
+        print(" ",end=" ")
+    for j in range(i):
+        if j==0 or i==n - 1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(i + 1):
+        if i==j or i==n - 1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
