125f566ec42b0536469129dc2cd9548aa8f3d36c
Abhi Sarkar <abhisarkar6038@gmail.com>
2025-05-05 14:09:13 +0530

    Update 02_problem112.py

--- Exercise 12/02_problem112.py
+++ Exercise 12/02_problem112.py
@@ -5,6 +5,7 @@ def print_params(x,y,z = 3, *pospar,**keypar):
     print(pospar)
     print(keypar)
 
+
 print_params(1,2,3,5,6,7, foo=1, bar=2)
 print_params(1,2)
 print_params(1,2,3,'Testing', foo=1, bar=2)
