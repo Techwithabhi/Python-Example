1c0616a0243c9839ce568d454166fe90a7fa9674
Abhi Sarkar <abhisarkar6038@gmail.com>
2025-05-09 09:16:02 +0530

    Update 10_problem50.py

--- Exercise 5/10_problem50.py
+++ Exercise 5/10_problem50.py
@@ -6,10 +6,13 @@ num2 = int(input("Enter 2nd number: "))
 def gcd(a, b):
     if a == 0 or a == b:
         return b
+        
     elif b == 0:
         return a
+        
     elif a > b :
         return gcd(a -b, b)
+        
     else:
         return gcd(a, b-a)
     
